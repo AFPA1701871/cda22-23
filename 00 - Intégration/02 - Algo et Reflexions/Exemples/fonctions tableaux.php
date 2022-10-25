ffe327bfe6dd216b0878f3b1478f98c975e2f767
AFPA1701871 <martine.poix@afpa.fr>
2022-10-25 08:49:28 +0200

    function afficheTableau bis

--- 00 - Intégration/02 - Algo et Reflexions/Exemples/fonctions tableaux.php
+++ 00 - Intégration/02 - Algo et Reflexions/Exemples/fonctions tableaux.php
@@ -1,7 +1,7 @@
 <?php
 
 //$tab = CreerTableau(2,3);
-$tab=[[1,2,3],[4,5,6]];
+$tab = [[1, 2, 3], [4, 5, 6]];
 //print_r($tab);
 AfficheTableau($tab);
 //AfficherGrille($tab);
@@ -21,37 +21,67 @@ function SaisieEntier($invite)
     return $valeur;
 }
 
-
-function CreerTableau($x, $y){
-    for($i=0; $i<$x;$i++){
-        for($j=0; $j<$y;$j++){
-            $lignes[$i][$j]= SaisieEntier("Veuillez entrer un chiffre : ");
+function CreerTableau($x, $y)
+{
+    for ($i = 0; $i < $x; $i++)
+    {
+        for ($j = 0; $j < $y; $j++)
+        {
+            $lignes[$i][$j] = SaisieEntier("Veuillez entrer un chiffre : ");
         }
     }
     return $lignes;
 }
 
-
 function AfficheTableau($tablo)
 {
-    $tiret="----"; //  pour prendre en compte le numero de ligne
-    $nomCol ="    ";
+    $nblignes = count($tablo);
     $nbCol = count($tablo[0]);
-    // on prepare les lignes de tirets + la ligne de nom de colonne
+    // on prepare les tirets et les entetes
+    $tirets ="+---+";
+    $entete = "    ";
     for ($i=0; $i < $nbCol; $i++) { 
-        $tiret .= "----";
-        $nomCol .= "| " . mb_chr(65+$i)." "; // on se sert du code ascii correspondant à la colonne
+       $tirets .= "---+";
+       $entete .= "| " . mb_chr($i+65) . " ";
     }
-    $tiret .="\n"; // on ajoute le retour à la ligne après les tirets
-
-    echo $nomCol."|\n".$tiret; // on met l'entete de colonne + un | pour finir la ligne
-    foreach ($tablo as $numLigne=>$ligne) {
-        // traitement ligne
-        echo "| ".($numLigne+1) ." "; // gestion du numéro de ligne
-        foreach ($ligne as  $case) {
-            // traitement de chaque case
-           echo "| " .$case." ";// on ajoute à gauche de chaque valeur un |
+    // ligne entete + tirets
+    echo $entete ."|\n".$tirets."\n";
+    // traitement du tableau
+    for ($i = 0; $i < $nblignes; $i++)
+    {
+        // traitement d'une ligne
+        // debut de ligne
+        echo "| " . ($i + 1) . " ";
+        for ($j = 0; $j < $nbCol; $j++)
+        {
+            //traitement d'une colonnne
+            echo "| " . $tablo[$i][$j] . " ";
         }
-        echo "|\n". $tiret;
+        // fin de ligne
+        echo "|\n". $tirets."\n";
     }
 }
+
+// function AfficheTableau($tablo)
+// {
+//     $tiret="----"; //  pour prendre en compte le numero de ligne
+//     $nomCol ="    ";
+//     $nbCol = count($tablo[0]);
+//     // on prepare les lignes de tirets + la ligne de nom de colonne
+//     for ($i=0; $i < $nbCol; $i++) {
+//         $tiret .= "----";
+//         $nomCol .= "| " . mb_chr(65+$i)." "; // on se sert du code ascii correspondant à la colonne
+//     }
+//     $tiret .="\n"; // on ajoute le retour à la ligne après les tirets
+
+//     echo $nomCol."|\n".$tiret; // on met l'entete de colonne + un | pour finir la ligne
+//     foreach ($tablo as $numLigne=>$ligne) {
+//         // traitement ligne
+//         echo "| ".($numLigne+1) ." "; // gestion du numéro de ligne
+//         foreach ($ligne as  $case) {
+//             // traitement de chaque case
+//            echo "| " .$case." ";// on ajoute à gauche de chaque valeur un |
+//         }
+//         echo "|\n". $tiret;
+//     }
+// }
