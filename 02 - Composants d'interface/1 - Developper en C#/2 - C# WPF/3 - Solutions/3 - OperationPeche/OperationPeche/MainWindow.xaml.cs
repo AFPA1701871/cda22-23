ffe1fed73b9cf060f6b3ad52a0ee1e9b6db1347e
afpa1701871 <martine.poix@afpa.fr>
2022-12-08 14:21:42 +0100

    solutions WPF

---
+++ 02 - Composants d'interface/1 - Developper en C#/2 - C# WPF/3 - Solutions/3 - OperationPeche/OperationPeche/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OperationPeche
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<Carac> users = new List<Carac>();
+            users.Add(new Carac() {  Caracteristique = "Déroulement de l'activité", Valeur = "Normal", Unite="aucune",Support="navire",Fraction="totale", Methode="Observation par Observateur" });
+            users.Add(new Carac() {  Caracteristique = "Etat de la mer", Valeur = "2 - belle, vagues", Unite = "aucune", Support = "masse d'eau", Fraction = "totale", Methode = "Observation par Observateur" });
+            users.Add(new Carac() { Caracteristique = "Opération diurne (jour)?", Unite = "aucune", Support = "opération", Fraction = "totale", Methode = "Observation par Observateur" }); 
+            users.Add(new Carac() { Caracteristique = "Nature du fond", Unite = "aucune", Support = "substrat", Fraction = "totale", Methode = "Déclaration d'un professionnel" });
+
+            Caracteristiques.ItemsSource = users;
+        }
+        class Carac
+        {
+            public string Caracteristique{ get; set; }
+            public string Valeur { get; set; }
+            public string Unite { get; set; }
+            public string Support { get; set; }
+            public string Fraction { get; set; }
+            public string Methode { get; set; }
+
+        }
+
+       
+    }
+}
