dae788e60c7a53787696cf38d58980fe737841c0
AFPA1701871 <martine.poix@afpa.fr>
2022-12-14 16:55:53 +0100

    calculatrice version 0

--- 02 - Composants d'interface/1 - Developper en C#/2 - C# WPF/Exemples/Calculatrice/Calculatrice/MainWindow.xaml.cs
+++ 02 - Composants d'interface/1 - Developper en C#/2 - C# WPF/Exemples/Calculatrice/Calculatrice/MainWindow.xaml.cs
@@ -20,20 +20,101 @@ namespace Calculatrice
     /// </summary>
     public partial class MainWindow : Window
     {
+        Double op1, op2, resultat;
+        string op = "";
+
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void btnNum_Click(object sender, RoutedEventArgs e)
+        private void Button_Num_Click(object sender, RoutedEventArgs e)
         {
-            lblResultat.Content = (string)lblResultat.Content + ((Button)sender).Content;
+            saisie.Text += ((Button)sender).Content;
+            historique.Text += ((Button)sender).Content;
+        }
+        private void Button_PlusMoins_Click(object sender, RoutedEventArgs e)
+        {
+            saisie.Text += "-";
+            historique.Text += "-";
         }
 
-        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+
+        private void Button_Clear_Click(object sender, RoutedEventArgs e)
         {
-            lblResultat.Content = ((Window)sender).ActualHeight + " " + ((Window)sender).ActualWidth;
-            btn1.FontSize = ((Window)sender).ActualWidth / 20;
+            op = "";
+            op1 = 0;
+            op2 = 0;
+            saisie.Text = "";
+            historique.Text = "";
+        }
+
+
+
+        private void Button_Operateur_Click(object sender, RoutedEventArgs e)
+        {
+            if (op != "")
+            {
+                if (Calcul())
+                {
+                    // si op2 est une vrai opérande, le calcul a réussi
+                    // on met à jour l'historique
+                    historique.Text = resultat.ToString();
+                    // on met à jour la saisie pour permettre la mise à jour de op1 ligne 71
+                    saisie.Text = resultat.ToString();
+                }
+                else
+                {
+                    // si op2 est un autre opérateur
+                    // on retire l'operateur précédent et on recharge la saisie pour permettre la mise à jour de op1 ligne 71
+                    historique.Text = historique.Text.Remove(historique.Text.Length - 1);
+                    saisie.Text = op1.ToString();
+                }
+
+            }
+            op1 = Double.Parse(saisie.Text);
+            op = (string)((Button)sender).Content;
+            saisie.Text = "";
+            historique.Text += op;
+        }
+
+        
+
+        private void Button_Egal_Click(object sender, RoutedEventArgs e)
+        {
+            if (Calcul())
+            {
+                historique.Text += "= " + resultat;
+                saisie.Text = resultat.ToString();
+            }
+        }
+        private bool Calcul()
+        {
+            // tryParse : tente de convertir si ca marche parseOk vaut vrai et op2 vaut la valeur convertie
+            // sinon parseOk vaut false
+            bool parseOk = Double.TryParse(saisie.Text, out Double op2);
+            if (parseOk)
+            {
+                switch (op)
+                {
+                    case "+":
+                        resultat = op1 + op2;
+                        break;
+                    case "-":
+                        resultat = op1 - op2;
+                        break;
+                    case "*":
+                        resultat = op1 * op2;
+                        break;
+                    case "/":
+                        resultat = op1 / op2;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return parseOk;
         }
     }
 }
